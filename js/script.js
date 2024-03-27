2fda2df60d9cf76639412208d5e4ad024a0f5043
vegalindh <151616949+vegalindh@users.noreply.github.com>
2024-03-27 13:32:10 +0100

    v13
    yayayaya

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 console.log('testing')
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.querySelector(".menu-toggle");
+    const navMenu = document.querySelector("nav ul");
+  
+    toggleButton.addEventListener("click", function () {
+      navMenu.classList.toggle("menu-open");
+      navMenu.classList.toggle("vertical");
+    });
+  });
+  
+  var elem = document.querySelector(".m-p-g");
+  document.addEventListener("DOMContentLoaded", function () {
+    var gallery = new MaterialPhotoGallery(elem);
+  });
